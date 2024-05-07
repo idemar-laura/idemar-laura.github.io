e32ad76b9b280529bbd38c4967a8c84a49df8e4d
Idemar Neto <idemarneto13@gmail.com>
2024-05-06 21:16:39 -0300

    Merge branch 'main' of https://github.com/idemar-laura/idemar-laura.github.io

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// const btn = document.querySelector('input');
-
-// btn.addEventListener('click', btnChange);
-
-// function btnChange() {
-//     document.getElementById('love').style.display = 'flex';
-//     btn.style.display = 'none';
-// }
-
 function calcularDiferencaTempo(dataReferencia) {
     const dataAtual = new Date();
 
